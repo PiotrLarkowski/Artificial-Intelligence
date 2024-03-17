a90710230fd2627223ea34c2bc0d7c9c055fec30
Piotr <P.Larkowski90@gmail.com>
2024-03-17 21:56:41 +0100

    Add list of Matters in main class

--- src/Matter.java
+++ src/Matter.java
@@ -4,9 +4,10 @@ public class Matter {
     private String[] importantForMatter = new String[10];
     public Matter(String newMatter){
         this.newMatter = newMatter;
-        importantForMatter[0] = "Job";
-        importantForMatter[1] = "Alarm";
-        importantForMatter[2] = java.time.LocalTime.now().toString();
+        importantForMatter[0] = "Why - Job";
+        importantForMatter[1] = "How - Alarm";
+        importantForMatter[2] = "When - " +java.time.LocalTime.now().toString();
+        Person.person.listOfMatters.add(this);
     }
 
     public int getWeight() {
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
+    static Person person = new Person();
+    public List<Matter> listOfMatters = new ArrayList<>();
+
 }
